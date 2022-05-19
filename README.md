a1a450cb694cdb96e19d041935a7ca7fa28e546b
Ju-yeop <dhwnduq12@naver.com>
2022-05-19 21:55:08 +0900

    AuthSevice

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-22.05.16 React, Firebase Setup
-Firebase App User 추가
+#22.05.16 React, Firebase Setup
+#22.05.19 Login, Navigation Bar implementation completed
--- weight_time/src/routes/Auth.js
+++ weight_time/src/routes/Auth.js
@@ -1,4 +1,29 @@
 import React from "react";
+import { auth } from "../firebase";
+import {
+    FacebookAuthProvider,
+    GoogleAuthProvider,
+    signInWithPopup
+    } from "firebase/auth";
+const Auth = () => {
+    const onSocialClick = async (event) => {
+        const {target: { name }} = event;
+        let provider;
+        if (name === "google") {
+            provider = new GoogleAuthProvider();
+        } else if (name === "facebook") {
+            provider = new FacebookAuthProvider();
+        }
+        const data = await signInWithPopup(auth, provider);
+        console.log(data);
+    };
+    
+    return(
+            <div>
+                <button name="google" onClick={onSocialClick}>Continue with Google</button>
+                <button name="facebook" onClick={onSocialClick}>Continue with FaceBook</button>
+            </div>
+    )
+}
 
-const Auth = () => <span>Auth</span>;
 export default Auth;
