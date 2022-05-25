4e51cfba2a0739091ad515c101c9a028d4a02631
weightsforfun <giyoung914@gmail.com>
2022-05-25 15:30:33 +0900

    never mind

--- weight_time/src/routes/TimetablePage.js
+++ weight_time/src/routes/TimetablePage.js
@@ -45,73 +45,76 @@ const TimetablePage = () => {
   return (
     <div className="TimetablePage">
       <h1>Timetable</h1>
-      <form onSubmit={onSubmit}>
-        <select name="요일" onChange={onChangeDay}>
-          <option value={"월요일"}>월요일</option>
-          <option value={"화요일"}>화요일</option>
-          <option value={"수요일"}>수요일</option>
-          <option value={"목요일"}>목요일</option>
-          <option value={"금요일"}>금요일</option>
-          <option value={"토요일"}>토요일</option>
-          <option value={"일요일"}>일요일</option>
-        </select>
-        <label for="startTime">start time</label>
-        <input
-          type="number"
-          min="6"
-          max="22"
-          value={startTime}
-          id="startTime"
-          onChange={onChangeStartTime}
-        ></input>
-        <label for="endTime">end time</label>
-        <input
-          type="number"
-          min="6"
-          max="22"
-          value={endTime}
-          id="endTime"
-          onChange={onChangeEndTime}
-        ></input>
-        <label for="1">back</label>
-        <input
-          type="checkbox"
-          value="등"
-          id="1"
-          onChange={onChangePart}
-        ></input>
-        <label for="2">chest</label>
-        <input
-          type="checkbox"
-          value="가슴"
-          id="2"
-          onChange={onChangePart}
-        ></input>
-        <label for="3">arm</label>
-        <input
-          type="checkbox"
-          value="팔"
-          id="3"
-          onChange={onChangePart}
-        ></input>
-        <label for="4">shoulder</label>
-        <input
-          type="checkbox"
-          value="어깨"
-          id="4"
-          onChange={onChangePart}
-        ></input>
-        <label for="5">leg</label>
-        <input
-          type="checkbox"
-          value="하체"
-          id="5"
-          onChange={onChangePart}
-        ></input>
-        <button type="submit" onSubmit={onSubmit}>
-          제출
-        </button>
-      </form>
+      <div className="submitBox">
+        <form onSubmit={onSubmit}>
+          <select name="요일" onChange={onChangeDay}>
+            <option value={"월요일"}>월요일</option>
+            <option value={"화요일"}>화요일</option>
+            <option value={"수요일"}>수요일</option>
+            <option value={"목요일"}>목요일</option>
+            <option value={"금요일"}>금요일</option>
+            <option value={"토요일"}>토요일</option>
+            <option value={"일요일"}>일요일</option>
+          </select>
+          <label for="startTime">start time</label>
+          <input
+            type="number"
+            min="6"
+            max="22"
+            value={startTime}
+            id="startTime"
+            onChange={onChangeStartTime}
+          ></input>
+          <label for="endTime">end time</label>
+          <input
+            type="number"
+            min="6"
+            max="22"
+            value={endTime}
+            id="endTime"
+            onChange={onChangeEndTime}
+          ></input>
+          <label for="1">back</label>
+          <input
+            type="checkbox"
+            value="등"
+            id="1"
+            onChange={onChangePart}
+          ></input>
+          <label for="2">chest</label>
+          <input
+            type="checkbox"
+            value="가슴"
+            id="2"
+            onChange={onChangePart}
+          ></input>
+          <label for="3">arm</label>
+          <input
+            type="checkbox"
+            value="팔"
+            id="3"
+            onChange={onChangePart}
+          ></input>
+          <label for="4">shoulder</label>
+          <input
+            type="checkbox"
+            value="어깨"
+            id="4"
+            onChange={onChangePart}
+          ></input>
+          <label for="5">leg</label>
+          <input
+            type="checkbox"
+            value="하체"
+            id="5"
+            onChange={onChangePart}
+          ></input>
+          <button type="submit" onSubmit={onSubmit}>
+            제출
+          </button>
+        </form>
+      </div>
+      <div className="selectTableBox"></div>
     </div>
   );
 };
