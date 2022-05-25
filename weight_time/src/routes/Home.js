9cf78c808a306894000ee67f68f0c0e125f5e962
Chan <danieltls@naver.com>
2022-05-25 13:42:38 +0900

    change_home_login_design

--- weight_time/src/routes/Home.js
+++ weight_time/src/routes/Home.js
@@ -7,7 +7,7 @@ const Home = ({ isLoggedIn }) => {
     <div className="Home" class="main_dis">
       <h1 class="mar_auto">Weight Time</h1>
       <button type="button" class="log_in">Primary</button>
-      <button class="mar_auto"> <Link to={"/Timetable"}>Check Now</Link></button>
+      {isLoggedIn ? <h1>반가워요 신찬규님</h1> : <h1>로그인해 시발</h1>}
       <div class="m_nav">
         <Navigation isLoggedIn={isLoggedIn} />
       </div>
