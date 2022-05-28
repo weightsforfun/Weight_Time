46bfa6006203bd292e1ec2e6ab1394a3078a464f
Chan <danieltls@naver.com>
2022-05-29 01:45:07 +0900

    design home partner group pt

--- weight_time/src/components/Navigation.js
+++ weight_time/src/components/Navigation.js
@@ -7,25 +7,22 @@ const Navigation = ({ isLoggedIn }) => {
     <div>
       <div className="nav1">
         <div className="top_c">
-          <Link to={"/Partner"}>파트너</Link>
+          <img src="img/home.png" /><br />
+          <Link to={"/Home"} style={{ textDecoration: "none", color: "white" }}>홈</Link>
         </div>
         <div className="top_c">
-          <Link to={"/TimetablePage"}>시간표</Link>
+          <img src="img/partner.png" /><br />
+          <Link to={"/Partner"} style={{ textDecoration: "none", color: "white" }}>파트너</Link>
         </div>
         <div className="top_c">
-          <Link to={"/Trainer"}>트레이너</Link>
+          <img src="img/timetable.png" /><br />
+          <Link to={"/TimetablePage"} style={{ textDecoration: "none", color: "white" }}>시간표</Link>
         </div>
-        {isLoggedIn ? (
-          <div>
-            <Link to={"/"} onClick={onLogOutClick}>
-              로그아웃
-            </Link>
-          </div>
-        ) : (
-          <div>
-            <Link to={"/Auth"}>로그인</Link>
-          </div>
-        )}
+        <div className="top_c">
+          <img src="img/trainer.png" /><br />
+          <Link to={"/Trainer"} style={{ textDecoration: "none", color: "white" }}>트레이너</Link>
+        </div>
+
       </div>
     </div>
   );
--- weight_time/src/routes/Home.js
+++ weight_time/src/routes/Home.js
@@ -7,8 +7,12 @@ const Home = ({ isLoggedIn }) => {
   return (
 
     <div className="main_dis">
-      <img className="logo" src="img/logo_w.png" /><br />
-      <button type="button" className="log_in">Primary</button>
+      <div className="logo_div">
+        <img className="logo" src="img/logo_w.png" />
+        <button type="button" className="log_in">로그인</button>
+      </div>
+
+
       {isLoggedIn ? <h1>반가워요 신찬규님</h1> : <h1>로그인해 시발</h1>}
       <div className="m_nav">
         <Navigation isLoggedIn={isLoggedIn} />
@@ -16,24 +20,35 @@ const Home = ({ isLoggedIn }) => {
       <div className="famous_saying">
         <p><strong>우리가 늙어서 운동을 그만두는 것이 아니라,
           우리가 운동을 그만두기 때문에 늙는 것이다.</strong><br />
-          <span style={{ color: "#c4c4c4", fontStyle: "italic" }}>-Kenneth Cooper-</span>
+          <span className="famous_people">-Kenneth Cooper-</span>
         </p>
+      </div>
+      <div className="famous_saying">
         <p><strong>사람이 자신의 몸이 가질 수 있는 아름다움과 강함을 알지 못하고
-          늙어 버리는 것은<br /> 안타까운 일이다.</strong><br />
-          <span style={{ color: "#c4c4c4", fontStyle: "italic" }}>-Socrates-</span>
+          늙어 버리는 것은 안타까운 일이다.</strong><br />
+          <span className="famous_people">-Socrates-</span>
         </p>
-        <p><strong>운동은 사람의 몸과 감정과 정신력의 <br />창조적 변화를 위한 약이다.</strong><br />
-          <span style={{ color: "#c4c4c4", fontStyle: "italic" }}>-Carol Welch-</span>
+      </div>
+      <div className="famous_saying">
+        <p><strong>운동은 사람의 몸과 감정과 정신력의 창조적 변화를 위한 약이다.</strong><br />
+          <span className="famous_people">-Carol Welch-</span>
         </p>
-        <p><strong>운동은 몸의 건강을 위해 가장 중요한 것일 뿐 아니라, 다이나믹하고 창조적인 지적 활동을<br /> 위한 기반이기도 하다.</strong><br />
-          <span style={{ color: "#c4c4c4", fontStyle: "italic" }}>-John F.Kennedy-</span>
+      </div>
+      <div className="famous_saying">
+        <p><strong>운동은 몸의 건강을 위해 가장 중요한 것일 뿐 아니라, 다이나믹하고 창조적인 지적 활동을 위한 기반이기도 하다.</strong><br />
+          <span className="famous_people">-John F.Kennedy-</span>
         </p>
-        <p><strong>운동을 해야한다.<br />그렇지 않으면 어느 순간 <br />당신은 고장 날 것이다</strong><br />
-          <span style={{ color: "#c4c4c4", fontStyle: "italic" }}>-Barack Obama-</span>
+      </div>
+      <div className="famous_saying">
+        <p><strong>운동을 해야한다. 그렇지 않으면 어느 순간 당신은 고장 날 것이다</strong><br />
+          <span className="famous_people">-Barack Obama-</span>
         </p>
-        <p><strong>운동을 위해 시간을 내지 않으면,<br /> 병 때문에 시간을 내야하게 될지도 모른다.</strong><br />
-          <span style={{ color: "#c4c4c4", fontStyle: "italic" }}>-Robin Sharma-</span>
+      </div>
+      <div className="famous_saying">
+        <p><strong>운동을 위해 시간을 내지 않으면, 병 때문에 시간을 내야하게 될지도 모른다.</strong><br />
+          <span className="famous_people">-Robin Sharma-</span>
         </p>
+
       </div>
     </div>
 
