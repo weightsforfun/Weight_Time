a3493a5d2f76a374a1d722a3bff70510d2ca1c2f
weightsforfun <giyoung914@gmail.com>
2022-05-30 20:36:44 +0900

    ui updated is done

--- weight_time/src/components/Timetable.js
+++ weight_time/src/components/Timetable.js
@@ -26,6 +26,8 @@ class Appi extends Component {
                 ...defaultAttributes.style,
                 textAlign: "center",
                 textDecoration: "underline",
+                background: "rgba(100,100,100,0)",
+                color: "black",
               }}
             >
               {hour}
